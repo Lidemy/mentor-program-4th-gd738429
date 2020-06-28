1eaf863a64758d874431143a1a436e3f461d333e
張皓銓 <>
2020-06-28 17:34:23 +0800

    hw1~hw3 ok

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,5 +1,8 @@
 function printStars(n) {
-  
+  	for(i = 0; i < n; i++){
+  		console.log('*')
+  	}
 }
 
 printStars(5);
+
--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,17 @@
 function capitalize(str) {
-  
+  if(isFirstCharEnglish(str)){
+  	var result = ''
+  	result =  str.replace(str[0], str[0].toUpperCase())
+  	return result
+  }
+  return str
 }
 
-console.log(capitalize('hello'));
+function isFirstCharEnglish(str){
+	if(str.charCodeAt(0) >= 'A'.charCodeAt(0) && str.charCodeAt(0) <= 'z'.charCodeAt(0)){
+		return 1
+	}
+	return 0
+}
+
+console.log(capitalize('nick'));
