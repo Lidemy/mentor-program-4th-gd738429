645abce7a6687c6b011d9bc5e5043a0a89d597de
張皓銓 <>
2020-07-07 08:08:22 +0800

    Week3 hw3 ok

--- homeworks/week3/hw3.js
+++ homeworks/week3/hw3.js
@@ -1,48 +1,43 @@
-var readline = require('readline');
+const readline = require('readline');
 
-var lines = []
-var rl = readline.createInterface({
-  input: process.stdin
+const lines = [];
+const rl = readline.createInterface({
+  input: process.stdin,
 });
 
-rl.on('line', function (line) {
-  lines.push(line)
-});
+function isPrime(n) {
+  // 如果 n = 1
+  if (n === 1) {
+    console.log('Composite');
+    return;
+  }
+  if (n === 2) {
+    console.log('Prime');
+    return;
+  }
+  // 用迴圈判斷是否被 1 以外的數整除,是 => Composite ,否 => Prime
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) {
+      console.log('Composite');
+      return;
+    }
+  }
+  console.log('Prime');
+}
 
-rl.on('close', function() {
-  solve(lines)
-})
-
-function solve(lines) {
-  let row = Number(lines[0])
+function solve(line) {
+  const row = Number(line[0]);
   // 迴圈跑 row 筆資料
-  for (let i = 1; i <= row; i++) {
-  	// 判斷邏輯
-  	isPrime(Number(lines[i]))
+  for (let i = 1; i <= row; i += 1) {
+    // 判斷邏輯
+    isPrime(Number(line[i]));
   }
 }
 
-function isPrime(n){
-	// 如果 n = 1
-	if(n === 1){
-		console.log('Composite')
-		return
-	}
-	if(n === 2){
-		console.log('Prime')
-		return
-	}
-	// 用迴圈判斷是否被 1 以外的數整除, 是 => Composite , 否 => Prime
-	for (let i = 2; i < n ; i++) {
-		if( n % i === 0){
-			console.log('Composite')
-			return
-		}
-	}
-	console.log('Prime')
-}
+rl.on('line', (line) => {
+  lines.push(line);
+});
 
-
-
-
-
+rl.on('close', () => {
+  solve(lines);
+});
