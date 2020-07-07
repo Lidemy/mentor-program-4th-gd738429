6f2a9e8da59f02adaef2ac64dfbbe763fb4bbec8
張皓銓 <>
2020-07-07 08:13:21 +0800

    Week3 hw4 ok

--- homeworks/week3/hw4.js
+++ homeworks/week3/hw4.js
@@ -1,27 +1,27 @@
-var readline = require('readline');
+const readline = require('readline');
 
-var lines = []
-var rl = readline.createInterface({
-  input: process.stdin
+const lines = [];
+const rl = readline.createInterface({
+  input: process.stdin,
 });
 
-rl.on('line', function (line) {
-  lines.push(line)
-});
-
-rl.on('close', function() {
-  solve(lines)
-})
-
-function solve(lines) {
-	let str = lines[0]
-	let tmpStr = ''
-	for (let i = str.length - 1; i >= 0; i--) {
-		tmpStr += str[i]
-	}
-	if(tmpStr === str){
-		console.log('True')
-	}else{
-		console.log('False')
-	}
+function solve(line) {
+  const str = line[0];
+  let tmpStr = '';
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    tmpStr += str[i];
+  }
+  if (tmpStr === str) {
+    console.log('True');
+  } else {
+    console.log('False');
+  }
 }
+
+rl.on('line', (line) => {
+  lines.push(line);
+});
+
+rl.on('close', () => {
+  solve(lines);
+});
