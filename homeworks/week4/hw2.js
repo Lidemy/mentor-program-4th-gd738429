a006c52375185931755be1be3d31ca614cd6d064
張皓銓 <>
2020-07-10 08:10:31 +0800

    Week3 HW2 ok

---
+++ homeworks/week4/hw2.js
@@ -0,0 +1,91 @@
+const request = require('request');
+const process = require('process');
+
+const input1 = process.argv[2];
+const input2 = process.argv[3];
+const input3 = process.argv[4];
+
+switch (input1) {
+  case 'list':
+    request(
+      'https://lidemy-book-store.herokuapp.com/books?_limit=20',
+      (err, response, body) => {
+        const json = JSON.parse(body);
+        for (let i = 0; i < json.length; i += 1) {
+          console.log(`${json[i].id} ${json[i].name}`);
+        }
+      },
+    );
+    break;
+
+  case 'read':
+    request(
+      `https://lidemy-book-store.herokuapp.com/books/${input2}`,
+      (err, response, body) => {
+        if (response.statusCode === 404) {
+          console.log('輸入格式有誤,請重新輸入');
+        } else {
+          const json = JSON.parse(body);
+          console.log(json.name);
+        }
+      },
+    );
+    break;
+
+  case 'delete':
+    request.delete(
+      `https://lidemy-book-store.herokuapp.com/books/${input2}`,
+      (err, response) => {
+        if (response.statusCode === 404) {
+          console.log('刪除格式有誤,請重新輸入');
+        } else {
+          console.log(`id:${input2} 刪除成功！！！`);
+        }
+      },
+    );
+    break;
+
+  case 'create':
+    request.post(
+      {
+        url: 'https://lidemy-book-store.herokuapp.com/books',
+        form: {
+          id: '',
+          name: input2,
+        },
+      },
+      (err, response, body) => {
+        if (response.statusCode === 404) {
+          console.log('新增格式有誤,請重新輸入');
+        } else {
+          const json = JSON.parse(body);
+          console.log(`新增成功：id:${json.id}, name:${json.name}`);
+        }
+      },
+    );
+    break;
+
+  case 'update':
+    request.patch(
+      {
+        url: `https://lidemy-book-store.herokuapp.com/books/${input2}`,
+        form: {
+          id: input2,
+          name: input3,
+        },
+      },
+      (err, response, body) => {
+        if (response.statusCode === 404) {
+          console.log('更新格式有誤,請重新輸入');
+        } else {
+          const json = JSON.parse(body);
+          console.log(`更新成功：id:${json.id}, name:${json.name}`);
+        }
+      },
+    );
+    break;
+
+  default:
+    console.log('輸入的參數不正確... 參數格式：[ list, read, delete, create, update ]');
+    break;
+}
