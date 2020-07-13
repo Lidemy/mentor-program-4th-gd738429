fa055a7e6fc1e1e63414fe0729df89634dcd20f0
張皓銓 <>
2020-07-14 03:41:03 +0800

    Week4 hw1 修正

--- homeworks/week4/hw1.js
+++ homeworks/week4/hw1.js
@@ -3,7 +3,13 @@ const request = require('request');
 request(
   'https://lidemy-book-store.herokuapp.com/books?_limit=10',
   (err, res, body) => {
-    const json = JSON.parse(body);
+    let json;
+    try {
+      json = JSON.parse(body);
+    } catch (e) {
+      console.log(e);
+    }
+
     for (let i = 0; i < json.length; i += 1) {
       console.log(`${json[i].id} ${json[i].name}`);
     }
