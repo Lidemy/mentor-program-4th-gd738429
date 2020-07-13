842132db6b7b18f4bc2d5588dac0e33182f32eb7
張皓銓 <>
2020-07-14 04:01:00 +0800

    Week4 HW3 作業檢討

--- homeworks/week4/hw3.js
+++ homeworks/week4/hw3.js
@@ -12,14 +12,17 @@ request(
     } catch (e) {
       console.log(e);
     }
-    for (let i = 0; i < json.length; i += 1) {
-      console.log(
-        `============
+
+    if (response.statusCode >= 200 && response.statusCode < 300) {
+      for (let i = 0; i < json.length; i += 1) {
+        console.log(
+          `============
 國家：${json[i].name}
 首都：${json[i].capital}
 貨幣：${json[i].currencies.code}
 國碼：${json[i].callingCodes[0]}`,
-      );
+        );
+      }
     }
   },
 );
